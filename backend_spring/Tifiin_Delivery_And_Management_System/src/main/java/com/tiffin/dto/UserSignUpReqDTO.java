4c31fd8d08461ef46b27c38bc80b280f55e521a2
screamylicious <akumbhare47@gmail.com>
2024-08-06 18:35:06 +0530

    sign up functionality added

---
+++ backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/dto/UserSignUpReqDTO.java
@@ -0,0 +1,37 @@
+package com.tiffin.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.tiffin.enums.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserSignUpReqDTO {
+	
+//		@JsonProperty(access = Access.READ_ONLY) // this property only used during ser.
+//		private Long id;
+		@NotBlank(message = "First Name required")
+		private String firstName;
+		private String lastName;
+		@Email(message = "Invalid Email!!!")
+		private String email;
+		@JsonProperty(access = Access.WRITE_ONLY)
+		private String password;
+//		private Role role;
+//		public CustomerSignUpReqDTO(String firstName, String lastName,
+//				String email, String password) {
+//			super();
+//			this.firstName = firstName;
+//			this.lastName = lastName;
+//			this.email = email;
+//			this.password = password;
+//			this.role = Role.ROLE_CUSTOMER;
+//		}
+}
--- backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/service/UserServiceImpl.java
+++ backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/service/UserServiceImpl.java
@@ -1,12 +1,23 @@
 package com.tiffin.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.tiffin.custom_exceptions.ResourceNotFoundException;
+import com.tiffin.dto.AddressReqDTO;
 import com.tiffin.dto.ApiResponse;
+import com.tiffin.dto.UserSignUpReqDTO;
+import com.tiffin.dto.VendorSignUpReqDTO;
+import com.tiffin.entities.Address;
+import com.tiffin.entities.DeliveryBoy;
 import com.tiffin.entities.User;
+import com.tiffin.enums.DeliveryStatus;
+import com.tiffin.enums.Role;
+import com.tiffin.repository.DeliveryBoyRepository;
 import com.tiffin.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -16,16 +27,52 @@ import jakarta.transaction.Transactional;
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
-
-	@Override
-	public ApiResponse saveUser(User user) {
-		userRepository.save(user);
-		return new ApiResponse("New User Added!!!");
-	}
+	@Autowired
+	private DeliveryBoyRepository deliveryBoyRepository;
+	@Autowired
+	private ModelMapper mapper;
 
 	@Override
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
 
+	}
+
+	@Override
+	public ApiResponse saveCustomer(UserSignUpReqDTO user) {
+		User u = mapper.map(user, User.class);
+		u.setRole(Role.ROLE_CUSTOMER);
+		userRepository.save(u);
+		return new ApiResponse("New Customer Added!!!");
+	}
+
+	@Override
+	public ApiResponse saveDeliveryBoy(UserSignUpReqDTO deliveryBoy, AddressReqDTO address) {
+		User u = mapper.map(deliveryBoy, User.class);
+		u.setRole(Role.ROLE_DELIVERY_BOY);
+		u.addAddress(mapper.map(address, Address.class));
+		userRepository.save(u);
+		System.out.println(u.getId());
+		DeliveryBoy d = new DeliveryBoy(u, DeliveryStatus.AVAILABLE, address.getZipcode());
+		System.out.println(d);
+		deliveryBoyRepository.save(d);
+		return new ApiResponse("New Delivery Boy Added!!!");
+	}
+
+	@Override
+	public ApiResponse saveVendor(VendorSignUpReqDTO vendor, AddressReqDTO address) {
+		User u = mapper.map(vendor, User.class);
+		u.setRole(Role.ROLE_VENDOR);
+		u.addAddress(mapper.map(address, Address.class));
+		userRepository.save(u);
+		return new ApiResponse("New Vendor Added!!!");
+	}
+
+	@Override
+	public ApiResponse addCustomerAddresses(AddressReqDTO address, Long userId) {
+		User u = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("user does not exist"));
+		u.addAddress(mapper.map(address, Address.class));
+		return new ApiResponse("New Address Added!!!");
 	}
 }

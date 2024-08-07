8c41837ff64e31d0d5f0c03694ad37883a8ca947
screamylicious <akumbhare47@gmail.com>
2024-08-07 18:39:47 +0530

    image handling for menu item done

---
+++ backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/dto/MenuResWithImageDTO.java
@@ -0,0 +1,17 @@
+package com.tiffin.dto;
+
+import com.tiffin.enums.MenuCategory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MenuResWithImageDTO {
+    
+    private String name;
+    private String description;
+    private Double price;
+    private MenuCategory category;
+    private Integer quantity;
+    private byte[] menuImage;
+}

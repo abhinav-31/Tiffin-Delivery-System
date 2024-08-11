78273b2e433234f17f7015e5b64a178e7ef45373
D2-83772-Abhinav <abhinavmshr96@gmail.com>
2024-08-11 20:55:43 +0530

    spring security added

--- backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/security/CustomUserDetailsService.java
+++ backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/security/CustomUserDetailsService.java
@@ -1,30 +1,3 @@
-//package com.tiffin.security;
-//
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Component;
-//
-//import com.tiffin.entities.User;
-//import com.tiffin.repository.UserRepository;
-//
-//@Component
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//	@Autowired
-//	private UserRepository userRepository;
-//	
-//	@Override
-//	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//		User user =  userRepository.findByEmail(email);
-//		return new CustomUserDetails(user);
-//	}
-//
-//}
-
 package com.tiffin.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,27 +7,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import com.tiffin.entities.User;
-import com.tiffin.*;
 import com.tiffin.repository.UserRepository;
 
 @Service
 @Transactional
 public class CustomUserDetailsService implements UserDetailsService {
-	@Autowired
-	private UserRepository userRepo;
 
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		// invoke dao's method
-		User user = userRepo.findByEmail(email)
-				.orElseThrow(() -> new UsernameNotFoundException("Email not found !!!!!"));
-		// => user email exists - user : persistent
-		/*
-		 * In case of email found -- this method has to ret UserDetails object filled
-		 * with details lifted from DB
-		 */
-		return new CustomUserDetails(user);
-	}
+    @Autowired
+    private UserRepository userRepo;
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found!"));
+        return new CustomUserDetails(user);
+    }
 }
--- backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/security/JwtUtils.java
+++ backend_spring/Tifiin_Delivery_And_Management_System/src/main/java/com/tiffin/security/JwtUtils.java
@@ -1,13 +1,12 @@
 package com.tiffin.security;
 
-
-
 import java.security.Key;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,195 +15,60 @@ import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.SignatureAlgorithm;
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
-@Getter
 public class JwtUtils {
-	
-	@Value("${SECRET_KEY}")
-	private String jwtSecret;
 
-	@Value("${EXP_TIMEOUT}")
-	private int jwtExpirationMs;
-	
-	private Key key;
-	
-	@PostConstruct
+    @Value("${SECRET_KEY}")
+    private String jwtSecret;
+
+    @Value("${EXP_TIMEOUT}")
+    private int jwtExpirationMs;
+
+    private Key key;
+
+    @PostConstruct
     public void init() {
-        // Validate if jwtSecret is not null or empty
-        if (jwtSecret == null || jwtSecret.isEmpty()) {
-            throw new IllegalArgumentException("JWT Secret Key must be provided");
-        }
         key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-        log.info("JWT key initialized: " + key);
     }
-	
-	// will be invoked by Authentication controller) , upon successful
-	// authentication
-	public String generateJwtToken(Authentication authentication) {
-		log.info("generate jwt token " + authentication);
-		CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
-//JWT : userName,issued at ,exp date,digital signature(does not typically contain password , can contain authorities
-		return Jwts.builder() // JWTs : a Factory class , used to create JWT tokens
-				.setSubject((userPrincipal.getUsername())) // setting subject part of the token(typically user
-															// name/email)
-				.setIssuedAt(new Date())// Sets the JWT Claims iat (issued at) value of current date
-				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))// Sets the JWT Claims exp
-																					// (expiration) value.
-				// setting a custom claim
-				.claim("authorities", getAuthoritiesInString(userPrincipal.getAuthorities()))
-				.signWith(key, SignatureAlgorithm.HS512) // Signs the constructed JWT using the specified
-															// algorithm with the specified key, producing a
-															// JWS(Json web signature=signed JWT)
 
-				// Using token signing algo : HMAC using SHA-512
-				.compact();// Actually builds the JWT and serializes it to a compact, URL-safe string
-	}
-	
-//	public String getEmailFromJwtToken(String jwt) {
-////		jwt=jwt.substring(7);
-//		
-//		Claims claims=Jwts.parser().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-//		String email=String.valueOf(claims.get("email"));
-//		
-//		return email;
-//	}
-	
-	// this method will be invoked by our custom JWT filter
-		public String getUserNameFromJwtToken(Claims claims) {
-			return claims.getSubject();
-		}
-		
-//		public String getEmailFromJwtToken(Claims claims) {
-//			return 
-//		}
-		// this method will be invoked by our custom filter
-		public Claims validateJwtToken(String jwtToken) {
-			// try {
-			System.out.println(key);
-			Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(jwtToken).getBody();
-			
-			return claims;		
-		}
-		// Accepts Collection<GrantedAuthority> n rets comma separated list of it's
-		// string form
+    public String generateJwtToken(Authentication authentication) {
+        log.info("generate jwt token " + authentication);
+        CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
 
-		private String getAuthoritiesInString(Collection<? extends GrantedAuthority> authorities) {
-			String authorityString = authorities.stream().
-					map(authority -> authority.getAuthority())
-					.collect(Collectors.joining(","));
-			System.out.println(authorityString);
-			return authorityString;
-		}
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername()) // Set email as the subject
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .claim("authorities", getAuthoritiesInString(userPrincipal.getAuthorities()))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 
-		public List<GrantedAuthority> getAuthoritiesFromClaims(Claims claims) {
-			String authString = (String) claims.get("authorities");
-			List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(authString);
-			authorities.forEach(System.out::println);
-			return authorities;
-		}
-	
-	
+    public String getUserNameFromJwtToken(Claims claims) {
+        return claims.getSubject();
+    }
+
+    public Claims validateJwtToken(String jwtToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody(); // Ensure this does not return null or empty
+    }
+
+    private String getAuthoritiesInString(Collection<? extends GrantedAuthority> authorities) {
+        return authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+    }
+
+    public List<GrantedAuthority> getAuthoritiesFromClaims(Claims claims) {
+        String authString = claims.get("authorities", String.class);
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(authString);
+    }
 }
-
-//package com.tiffin.security;
-//
-//import java.security.Key;
-//import java.util.Collection;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//import jakarta.*;
-//import jakarta.annotation.PostConstruct;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.stereotype.Component;
-//
-//import io.jsonwebtoken.Claims;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import io.jsonwebtoken.security.Keys;
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Component
-//@Slf4j
-//public class JwtUtils {
-//
-//	@Value("${SECRET_KEY}")
-//	private String jwtSecret;
-//
-//	@Value("${EXP_TIMEOUT}")
-//	private int jwtExpirationMs;
-//
-//	private Key key;
-//
-//	@PostConstruct
-//	public void init() {
-//		key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-//	}
-//
-//	// will be invoked by Authentication controller) , upon successful
-//	// authentication
-//	public String generateJwtToken(Authentication authentication) {
-//		log.info("generate jwt token " + authentication);
-//		CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
-////JWT : userName,issued at ,exp date,digital signature(does not typically contain password , can contain authorities
-//		return Jwts.builder() // JWTs : a Factory class , used to create JWT tokens
-//				.setSubject((userPrincipal.getUsername())) // setting subject part of the token(typically user
-//															// name/email)
-//				.setIssuedAt(new Date())// Sets the JWT Claims iat (issued at) value of current date
-//				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))// Sets the JWT Claims exp
-//																					// (expiration) value.
-//				// setting a custom claim
-//				.claim("authorities", getAuthoritiesInString(userPrincipal.getAuthorities()))
-//				.signWith(key, SignatureAlgorithm.HS512) // Signs the constructed JWT using the specified
-//															// algorithm with the specified key, producing a
-//															// JWS(Json web signature=signed JWT)
-//
-//				// Using token signing algo : HMAC using SHA-512
-//				.compact();// Actually builds the JWT and serializes it to a compact, URL-safe string
-//	}
-//
-//	// this method will be invoked by our custom JWT filter
-//	public String getUserNameFromJwtToken(Claims claims) {
-//		return claims.getSubject();
-//	}
-//
-//	// this method will be invoked by our custom filter
-//	public Claims validateJwtToken(String jwtToken) {
-//		// try {
-//		@SuppressWarnings("deprecation")
-//		Claims claims = Jwts.parser().setSigningKey(key).build().
-//		// Sets the signing key used to verify JWT digital signature.
-//				parseClaimsJws(jwtToken).getBody();// Parses the signed JWT returns the resulting Jws<Claims> instance
-//		// throws exc in case of failures in verification
-//		return claims;
-//	}
-//	// Accepts Collection<GrantedAuthority> n rets comma separated list of it's
-//	// string form
-//
-//	private String getAuthoritiesInString(Collection<? extends GrantedAuthority> authorities) {
-//		String authorityString = authorities.stream().map(authority -> authority.getAuthority())
-//				.collect(Collectors.joining(","));
-//		System.out.println(authorityString);
-//		return authorityString;
-//	}
-//
-//	public List<GrantedAuthority> getAuthoritiesFromClaims(Claims claims) {
-//		String authString = (String) claims.get("authorities");
-//		List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(authString);
-//		authorities.forEach(System.out::println);
-//		return authorities;
-//	}
-//
-//}
